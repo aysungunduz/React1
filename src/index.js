dd3134a1ce30460663cedd9a8bc25d981c24c29d
aysun gunduz <113923715+aysungunduz@users.noreply.github.com>
2023-08-15 12:56:05 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Sushi</li>
+      <li>Noodles</li>
+      <li>Chicken</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
